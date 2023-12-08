86ad95457678adfc2a366b3da4f42a2358af8bee
Saymon Porras Briones <sporras@estudiantec.cr>
2023-12-08 00:03:09 -0600

    Arreglao

--- Proyecto1POO/src/main/MainMenu.java
+++ Proyecto1POO/src/main/MainMenu.java
@@ -295,7 +295,7 @@ public class MainMenu extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new MainMenu().setVisible(true);
+                new Login().setVisible(true);
             }
         });
     }
