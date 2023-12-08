5b76c831903b33597a6f0a06cf2eee0aeeeff5e0
Jose Mario Jiménez <jos-jimenez@estudiantec.cr>
2023-12-08 13:08:24 -0600

    clase products

---
+++ Proyecto1POO/src/main/products_class.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author josem
+ */
+public class products_class {
+    private String name;
+    private int code;
+        
+    public products_class(){}
+        
+    public void SearchProduct(){
+        System.out.println("MétodoBuscarProducto");
+    }
+}
