1e6eb09c764125cf084812d6a26e4f5efa7fcede
Jose Mario Jiménez <jos-jimenez@estudiantec.cr>
2023-12-19 11:08:20 -0600

    comments

--- Proyecto1POO/src/Classes/customer_class.java
+++ Proyecto1POO/src/Classes/customer_class.java
@@ -22,38 +22,84 @@ public class customer_class {
     private Date customer_birthdate;
     
     //Getter Methods
+    
+    /**
+    * Method to get the customer id.
+    * @author josem
+    * @return the getted customer_id
+    */
     public int getCustomer_id() {
         return customer_id;
     }
-
+    
+    /**
+    * Method to get the customer name.
+    * @author josem
+    * @return the getted customer_name
+    */
     public String getCustomer_name() {
         return customer_name;
     }
-
+    
+    /**
+    * Method to get the customer lastname.
+    * @author josem
+    * @return the getted customer_lastname
+    */
     public String getCustomer_lastname() {
         return customer_lastname;
     }
-
+    
+    /**
+    * Method to get the customer phone.
+    * @author josem
+    * @return the getted customer_phone
+    */
     public int getCustomer_phone() {
         return customer_phone;
     }
-
+    
+    /**
+    * Method to get the customer email.
+    * @author josem
+    * @return the getted customer_email
+    */
     public String getCustomer_email() {
         return customer_email;
     }
-
+    
+    /**
+    * Method to get the customer province.
+    * @author josem
+    * @return the getted customer_province
+    */
     public String getCustomer_province() {
         return customer_province;
     }
-
+    
+    /**
+    * Method to get the customer canton.
+    * @author josem
+    * @return the getted customer_canton
+    */
     public String getCustomer_canton() {
         return customer_canton;
     }
-
+    
+    /**
+    * Method to get the customer district.
+    * @author josem
+    * @return the getted customer_district
+    */
     public String getCustomer_district() {
         return customer_district;
     }
-
+    
+    /**
+    * Method to get the customer birthdate.
+    * @author josem
+    * @return the getted customer_birthdate
+    */
     public Date getCustomer_birthdate() {
         return customer_birthdate;
     }
@@ -61,38 +107,83 @@ public class customer_class {
     
     //Setter Methods
     
+    /**
+    * Method to set the customer id
+    * @author josem
+    * @param customer_id: The data to set in the object id
+    */
     public void setCustomer_id(int customer_id) {
         this.customer_id = customer_id;
     }
-
+    
+    /**
+    * Method to set the customer name
+    * @author josem
+    * @param customer_name: The data to set in the object name
+    */
     public void setCustomer_name(String customer_name) {
         this.customer_name = customer_name;
     }
-
+    
+    /**
+    * Method to set the customer lastname
+    * @author josem
+    * @param customer_lastname: The data to set in the object lastname
+    */
     public void setCustomer_lastname(String customer_lastname) {
         this.customer_lastname = customer_lastname;
     }
-
+    
+    /**
+    * Method to set the customer phone
+    * @author josem
+    * @param customer_phone: The data to set in the object phone
+    */
     public void setCustomer_phone(int customer_phone) {
         this.customer_phone = customer_phone;
     }
-
+    
+    /**
+    * Method to set the customer email
+    * @author josem
+    * @param customer_email: The data to set in the object email
+    */
     public void setCustomer_email(String customer_email) {
         this.customer_email = customer_email;
     }
-
+    
+    /**
+    * Method to set the customer province
+    * @author josem
+    * @param customer_province: The data to set in the object province
+    */
     public void setCustomer_province(String customer_province) {
         this.customer_province = customer_province;
     }
-
+    
+    /**
+    * Method to set the customer canton
+    * @author josem
+    * @param customer_canton: The data to set in the object canton
+    */
     public void setCustomer_canton(String customer_canton) {
         this.customer_canton = customer_canton;
     }
-
+    
+    /**
+    * Method to set the customer district
+    * @author josem
+    * @param customer_district: The data to set in the object district
+    */
     public void setCustomer_district(String customer_district) {
         this.customer_district = customer_district;
     }
-
+    
+    /**
+    * Method to set the customer birthdate
+    * @author josem
+    * @param customer_birthdate: The data to set in the object birthdate
+    */
     public void setCustomer_birthdate(Date customer_birthdate) {
         this.customer_birthdate = customer_birthdate;
     }
--- Proyecto1POO/src/Classes/item_class.java
+++ Proyecto1POO/src/Classes/item_class.java
@@ -14,69 +14,149 @@ public class item_class {
     
     //Setter Methods
     
+    /**
+    * Method to set the item id
+    * @author josem
+    * @param id_item: The data to set in the object id
+    */
     public void setId_item(int id_item) {
         this.id_item = id_item;
     }
-
+    
+    /**
+    * Method to set the item category
+    * @author josem
+    * @param category_item: The data to set in the object category
+    */
     public void setCategory_item(String category_item) {
         this.category_item = category_item;
     }
-
+    
+    /**
+    * Method to set the item name
+    * @author josem
+    * @param name_item: The data to set in the object name
+    */
     public void setName_item(String name_item) {
         this.name_item = name_item;
     }
-
+    
+    /**
+    * Method to set the item type
+    * @author josem
+    * @param type_item: The data to set in the object type
+    */
     public void setType_item(String type_item) {
         this.type_item = type_item;
     }
-
+    
+    /**
+    * Method to set the item size
+    * @author josem
+    * @param size_item: The data to set in the object size
+    */
     public void setSize_item(Double size_item) {
         this.size_item = size_item;
     }
-
+    
+    /**
+    * Method to set the item brand
+    * @author josem
+    * @param brand_item: The data to set in the object brand
+    */
     public void setBrand_item(String brand_item) {
         this.brand_item = brand_item;
     }
-
+    
+    /**
+    * Method to set the item price
+    * @author josem
+    * @param price_item: The data to set in the object price
+    */
     public void setPrice_item(int price_item) {
         this.price_item = price_item;
     }
-
+    
+    /**
+    * Method to set the item amount
+    * @author josem
+    * @param amount_item: The data to set in the object amount
+    */
     public void setAmount_item(int amount_item) {
         this.amount_item = amount_item;
     }
 
     
     //Getter Methods
-
+    
+    /**
+    * Method to get the item id.
+    * @author josem
+    * @return the getted item_id
+    */
     public int getId_item() {
         return id_item;
     }
-
+    
+    /**
+    * Method to get the item category.
+    * @author josem
+    * @return the getted item_category
+    */
     public String getCategory_item() {
         return category_item;
     }
-
+    
+    /**
+    * Method to get the item name.
+    * @author josem
+    * @return the getted item_name
+    */
     public String getName_item() {
         return name_item;
     }
-
+    
+    /**
+    * Method to get the item type.
+    * @author josem
+    * @return the getted item_type
+    */
     public String getType_item() {
         return type_item;
     }
-
+    
+    /**
+    * Method to get the item size.
+    * @author josem
+    * @return the getted item_size
+    */
     public Double getSize_item() {
         return size_item;
     }
-
+    
+    /**
+    * Method to get the item brand.
+    * @author josem
+    * @return the getted item_brand
+    */
     public String getBrand_item() {
         return brand_item;
     }
-
+    
+    /**
+    * Method to get the item price.
+    * @author josem
+    * @return the getted item_price
+    */
     public int getPrice_item() {
         return price_item;
     }
-
+    
+    /**
+    * Method to get the item amount.
+    * @author josem
+    * @return the getted item_amount
+    */
     public int getAmount_item() {
         return amount_item;
     }
